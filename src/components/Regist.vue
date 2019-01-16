434fdc95f815165eded7ab0daa14e61a41cb6572
steven <shi_liang_china@163.com>
2019-01-16 15:11:09 +0800

    登录注册页面

---
+++ src/components/Regist.vue
@@ -0,0 +1,18 @@
+<template>
+  <form action="/user/ulogin" method="post">
+    <br> <input type="text" name="username" />
+    <br> <input type="password" name="password" />
+    <br> <input type="submit" value="登陆" />
+    <button v-on:click="btnRegist">注册</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'Regist'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,36 @@
+<template>
+  <form>
+    <br> <input type="text" v-model="username" />
+    <br> <input type="password" v-model="password" />
+    <button v-on:click="btnLogin">登录</button>
+    <button v-on:click="btnRegist">注册</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'login',
+  methods: {
+    btnRegist: function () {
+      this.$router.push({path: '/regist'})
+    },
+    btnLogin: function () {
+      let formData = new FormData()
+      formData.append('username', this.username)
+      formData.append('password', this.password)
+      axios.post('http://localhost:8090/login', formData)
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
