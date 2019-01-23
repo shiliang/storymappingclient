4b4559877dee553d4321f974d76a09f38bde3281
steven <shi_liang_china@163.com>
2019-01-23 13:39:29 +0800

    登录注册跳到个人主页面

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.prototype.$axios = Axios
 
@@ -22,6 +24,8 @@ Axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded
   return Promise.reject(error)
 }) */
 /* eslint-disable no-new */
+Vue.use(ElementUI)
+
 new Vue({
   el: '#app',
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,12 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import login from '@/components/login'
 import Regist from '@/components/Regist'
+import Index from '@/components/Index'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -22,6 +24,11 @@ export default new Router({
       path: '/regist',
       name: 'regist',
       component: Regist
+    },
+    {
+      path: '/index/:userId',
+      name: 'Index',
+      component: Index
     }
   ]
 })
