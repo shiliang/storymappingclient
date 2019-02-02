78646d27b9375d60b34ac43e8719baf5e4f794b6
steven <shi_liang_china@163.com>
2019-02-02 11:25:25 +0800

    部署连接后台的地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"http://localhost:8090"',
+  BASE_API: '"http://47.102.135.76:8030/storymapping"',
 }
